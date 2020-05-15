cc6cdcd66f9292be617c509bda99dfaef1893c79
Kim Dotcom <kim.dotcom@email.cz>
2020-05-15 15:08:26 +0200

    camera/ET look angle and ET dataloss
    check for constant relative ET lookangle (ET dataloss)

--- RaycastTargetter.cs
+++ RaycastTargetter.cs
@@ -8,13 +8,19 @@ public class RaycastTargetter : MonoBehaviour
     //raycast source/target and settings
     public GameObject raySource;
     public GameObject rayDestination;
+    [Range(0f,10000f)]
     public float rayDistance = 500f;
+    [Range(0f,5f)]
+    public float rayDataLossAngleMax = 0f;
     [Space(10)]
 
     //logging setup and intensity
     public bool logEvents;
     public bool logCoordinatesSource;
     public bool logCoordinatesTarget;
+    public bool logDataLoss;
+    [Space(10)]
+
     [Range(0f,5f)]
     public float logDelay = 0.01f;
     [Space(10)]
@@ -32,14 +38,21 @@ public class RaycastTargetter : MonoBehaviour
     private string dataformatEvents;
     private string dataformatSource;
     private string dataformatTarget;
+    private string dataformatLoss;
     private string dataformatConcatenated;
 
     //auxiliaries
-    private Vector3 sourceCoordinates;
-    private Vector3 targetCoordinates;
-    private Vector3 normalizedDirection;
+    private Vector3 sourceCoordinates;              //camera coordinates
+    private Vector3 targetCoordinates;              //raycast target coordinates (hit on collider in space)
+    private Vector3 normalizedDirection;            //source-target directional vector
+    private float raySourceTargetAngleDiff;         //dataloss auxiliary
+    private float raySourceTargetAngleDiffPrevious; //dataloss auxiliary (to compare current look angle to)
+    private Camera sourceCamera;                    //dataloss auxiliary - to get look angle
+    private Vector3 sourceCameraLookDirection;      //dataloss auxiliary - camera look direction
+    private Vector3 sourceEyetrackingDirection;     //dataloss auxiliary - camera ET look direction
     private bool initValidized;
     private bool initWaited;
+    private bool isDataLoss;
     private string separatorDecimal = ".";
     private string separatorItem = ",";
     private NumberFormatInfo numberFormat;
@@ -56,12 +69,19 @@ public class RaycastTargetter : MonoBehaviour
         {
             Debug.LogWarning("Logger failed to initialize. Check Logger GameObject (PathScript).");
         }
+        else if (logDataLoss && raySource.GetComponent<Camera>() == null)
+        {
+            Debug.LogWarning("Logger failed to initialize for data loss verification. " +
+                             "No Camera attached to the raySource object (it has to be).");
+        }
         //if so, get the coordinates and allow raycast processing
         else
         {
             //init coordinates
             initRaycastCoordinates();
             initValidized = true;
+            //init camera
+            if (logDataLoss) sourceCamera = raySource.GetComponent<Camera>();
             //init file format
             Dictionary<string, string> fileFormatDict = new Dictionary<string, string>();
             fileFormatDict = Logger.GetComponent<PathScript>().getLogFormat();
@@ -80,10 +100,13 @@ public class RaycastTargetter : MonoBehaviour
                                    "vectorX" + separatorItem + "vectorY" + separatorItem + "vectorZ";
                 dataformatTarget = "targetX" + separatorItem + "targetY" + separatorItem + "targetZ" + separatorItem +
                                    "sourceTargetDistance" + separatorItem + "sourceHitDistance";
+                dataformatLoss = "cameraLookDirection" + separatorItem + "eyetrackingLookDirection" + separatorItem +
+                                 "cameraEyetrackingAngleDifference" + separatorItem + "isConsideredDataLoss";
                 dataformatConcatenated = dataformatDefault;
                 if (logEvents) dataformatConcatenated += separatorItem + dataformatEvents;
                 if (logCoordinatesSource) dataformatConcatenated += separatorItem + dataformatSource;
                 if (logCoordinatesTarget) dataformatConcatenated += separatorItem + dataformatTarget;
+                if (logDataLoss) dataformatConcatenated += separatorItem + dataformatLoss;
                 dataformatConcatenated += "\r\n";
                 Logger.GetComponent<PathScript>().generateCustomFileNames(dataformatConcatenated, customLogfileName,
                                                                           gameObject.name);
@@ -154,6 +177,32 @@ public class RaycastTargetter : MonoBehaviour
                            Vector3.Distance(sourceCoordinates, targetCoordinates) + separatorItem +
                            hit.distance;
             }
+            //log dataloss estimates
+            if (logDataLoss)
+            {
+                //get look direction of camera center and of eye-tracking
+                sourceCameraLookDirection = sourceCamera.transform.forward;
+                sourceEyetrackingDirection = (targetCoordinates - sourceCoordinates).normalized;
+                //get their difference, compare if over threshold (usually enough as (current == previous) e.g. 0 diff)
+                raySourceTargetAngleDiff = Vector3.Angle(sourceCameraLookDirection, sourceEyetrackingDirection);
+                if (Mathf.Abs(raySourceTargetAngleDiff - raySourceTargetAngleDiffPrevious) <= rayDataLossAngleMax)
+                {
+                    isDataLoss = true;
+                }
+                else
+                {
+                    isDataLoss = false;
+                }
+
+                 if (logEvents || logCoordinatesSource || logCoordinatesTarget) logData += separatorItem;
+                 logData += sourceCameraLookDirection + separatorItem +
+                            sourceEyetrackingDirection + separatorItem +
+                            raySourceTargetAngleDiff + separatorItem +
+                            isDataLoss;
+                //to have an camera/et angle diff to check with next logDelay
+                //the idea behind this verification: when ET is not recoding it remains the same relative to camera
+                raySourceTargetAngleDiffPrevious = raySourceTargetAngleDiff;
+            }
             //log the data and wait till the next cycle
             //Debug.Log("RCTargetter sending this: " + logData + ", to this file: " + customLogfileName);
             if (customLogfile)
@@ -164,7 +213,6 @@ public class RaycastTargetter : MonoBehaviour
             {
                 Logger.GetComponent<PathScript>().logEventData(logData);
             }
-            
             yield return new WaitForSeconds(logDelay);
         }
     }
